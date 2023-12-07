dc50bf78d67fc271dc48566cccd9689e4d7a2fb2
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-12-07 21:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function debounce(callback, delay, immediate = false) {
-  
+  let timeoutId;
+
+  return function (...args) {
+    const executeCallback = () => {
+      callback.apply(this, args);
+    };
+
+    clearTimeout(timeoutId);
+
+    if (immediate && !timeoutId) {
+      executeCallback();
+    }
+
+    timeoutId = setTimeout(executeCallback, delay);
   }
   
   module.exports = debounce;
